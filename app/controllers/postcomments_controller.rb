1d63dacd5064e5733745a1af13bf3d0ca3b652ae
vasya <v.pyskach@gmail.com>
2017-11-13 12:20:12 +0200

    my first commit int myNewBlog

---
+++ app/controllers/postcomments_controller.rb
@@ -0,0 +1,17 @@
+class PostcommentsController < ApplicationController
+before_action :authenticate_user!
+
+  def create
+  	params[:postcomment][:post_id] = params[:post_id]
+  	params[:postcomment][:user_id] = current_user.id
+  	Postcomment.create(comment_params)
+  	redirect_to post_path(params[:post_id])
+  end
+
+
+private
+
+	def comment_params
+		params.require(:postcomment).permit(:post_id, :user_id, :comment)
+	end
+end
